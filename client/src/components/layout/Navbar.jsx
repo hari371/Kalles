4848102a780ca2c6fb23f5ddd5efa9ceeedfc53e
hari371 <hkpbadnawar@gmail.com>
2026-06-24 16:09:38 +0530

    Implemented navbar layout and navigation

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,30 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { navLinks } from "../../constants/navigation";
 
 function Navbar() {
   return (
-    <nav className="border-b">
-      <div className="container mx-auto flex items-center justify-between py-4">
-        
-        <img src="https://kalles-5.myshopify.com/cdn/shop/files/kalles.svg?v=1717404087&width=190" alt="" />
+    <nav className="border-b border-gray-200 bg-white shadow">
+      <div className="container-custom flex justify-between items-center py-4">
+          <Link to="/">
+            <img src="https://kalles-5.myshopify.com/cdn/shop/files/kalles.svg?v=1717404087&width=190" alt="Kalles Logo" />
+          </Link>
 
-        <ul className="flex gap-6">
-          {navLinks.map((link) => (
-            <li key={link.path}>
-              <Link to={link.path}>
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+           <ul className="flex items-center justify-center gap-6">
+            {navLinks.map((link) => (
+              <li key={link.path}>
+                <NavLink to={link.path}
+                  className={({ isActive }) =>
+                  isActive ? "text-blue-500 transition-colors ease-in-out duration-500" : "text-black hover:text-blue-300 transition-colors ease-in-out duration-500"
+                }>
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
 
-        <div className="flex gap-4">
-          <button>Search</button>
-          <button>Account</button>
-          <button>Wishlist</button>
-          <button>Cart</button>
-        </div>
-
+          <div className="flex gap-4">
+            <button>Search</button>
+            <button>Account</button>
+            <button>Wishlist</button>
+            <button>Cart</button>
+          </div>
       </div>
     </nav>
   );
